bed0ce2e9999928941f55bdf315ee44d47404be0
[jsh1304] <[jung3914a@gmail.com]>
2022-02-17 22:38:44 +0900

    220217

--- app/src/main/java/jj/appproject/quizapp/MainActivity.kt
+++ app/src/main/java/jj/appproject/quizapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package jj.appproject.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btn_start)
+        val etName : EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if(etName.text.isEmpty()){
+                Toast.makeText(this, "이름을 작성해주세요!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish() // 뒤로 가기 버튼 누를시 종료
+            }
+        }
     }
 }
