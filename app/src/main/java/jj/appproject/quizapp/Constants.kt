58d9d342da858ccc4ab159e7ac8d3c0a1349b425
[jsh1304] <[jung3914a@gmail.com]>
2022-02-22 21:43:14 +0900

    220222

--- app/src/main/java/jj/appproject/quizapp/Constants.kt
+++ app/src/main/java/jj/appproject/quizapp/Constants.kt
@@ -7,10 +7,10 @@ object Constants {
 
         val que1 = Question(
             1, "다음은 어느 나라일까요?",
-            R.drawable.ic_flag_of_southkorea,
-            "대한민국", "호주",
-            "프랑스", "오스트리아",
-            1
+            R.drawable.ic_flag_of_germany,
+            "영국", "독일",
+            "폴란드", "북아일랜드",
+            2
 
         )
         questionsList.add(que1)
@@ -34,6 +34,78 @@ object Constants {
 
         )
         questionsList.add(que3)
+
+        val que4 = Question(
+            4, "다음은 어느 나라일까요?",
+            R.drawable.ic_flag_of_southkorea,
+            "대한민국", "호주",
+            "프랑스", "오스트리아",
+            1
+
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5, "다음은 어느 나라일까요?",
+            R.drawable.ic_flag_of_belgium,
+            "루마니아", "벨기에",
+            "독일", "에콰도르",
+            2
+
+        )
+        questionsList.add(que5)
+
+        val que6 = Question(
+            6, "다음은 누구일까요?",
+            R.drawable.ic_player_parkjisung,
+            "박지성", "유해진",
+            "손흥민", "조원희",
+            1
+
+        )
+        questionsList.add(que6)
+
+        val que7 = Question(
+            7, "다음은 어느 나라일까요?",
+            R.drawable.ic_flag_of_fiji,
+            "영국", "호주",
+            "남수단", "피지",
+            4
+
+        )
+        questionsList.add(que7)
+
+        val que8 = Question(
+            8, "다음은 누구일까요?",
+            R.drawable.ic_player_parkyongtaek,
+            "정근우", "이재우",
+            "박용택", "이병규",
+            3
+
+        )
+        questionsList.add(que8)
+
+        val que9 = Question(
+            9, "다음은 누구일까요?",
+            R.drawable.ic_actor_yuain,
+            "윤찬영", "김선호",
+            "이동욱", "유아인",
+            4
+
+        )
+        questionsList.add(que9)
+
+        val que10 = Question(
+            10, "다음은 누구일까요?",
+            R.drawable.ic_player_leedaeho,
+            "이대호", "최지만",
+            "최준석", "김태균",
+            1
+
+        )
+        questionsList.add(que10)
+
+
 
         return questionsList
     }
